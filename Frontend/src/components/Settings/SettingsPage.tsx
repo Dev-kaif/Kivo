bb85a8213e70d40411e747766efe5230cef21e3b
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2026-02-17 18:56:34 +0530

    Update SettingsPage.tsx

--- Frontend/src/components/Settings/SettingsPage.tsx
+++ Frontend/src/components/Settings/SettingsPage.tsx
@@ -4,7 +4,7 @@ import { ResetPasswordCard, ProfileCard } from "./SettingsComponents";
 
 export const SettingsContent = () => {
     return (
-        <div className="space-y-8 w-full mx-auto px-8">
+        <div className="space-y-8 w-full max-w-3xl mx-auto px-4 sm:px-8">
             <ProfileCard />
             <ResetPasswordCard />
         </div>
